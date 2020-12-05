2590af25a5d5e7f54c8038a1ad060ef1e8d31b0b
leehyeokjae <leehyeokjae97@gmail.com>
2020-12-06 01:11:41 +0900

    ver 0.26

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const api_url = "https://korean-webtoon-hub-project.herokuapp.com/";
+var today = new Date();
+var week = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+
+get_webtoon_by_service(0);
 
 function ajax_get(url, callback) {
   //ajax 구현을 위한 함수
@@ -19,52 +23,59 @@ function ajax_get(url, callback) {
   xmlhttp.send();
 }
 
-var target_api = api_url + "mon";
-ajax_get(target_api, function (data) {
-  //document.getElementById("webtoon_contents").innerHTML = test;
-  for (i = 0; i < data.length; i++) {
-    if (data[i].service == 1) {
-      var webtoon_contents = document.getElementById("webtoon_contents");
-      var webtoon_link = document.createElement("a");
-      webtoon_link.href = data[i].url;
-      var new_dt = document.createElement("dt");
-      new_dt.classList.add("webtoon_container");
-      var new_div = document.createElement("div");
-      new_div.classList.add("square");
-      var img_container = document.createElement("div");
-      img_container.classList.add("content");
-      var img_text = document.createElement("div");
-      img_text.classList.add("icon");
-      var new_dd = document.createElement("dd");
-      new_dd.classList.add("webtoon_info");
-      if (data[i].img == null) {
-        img_container.innerHTML =
-          "<img src=img/noimg.jpg width=25% height=15%>";
-      } else {
-        img_container.innerHTML =
-          "<img style='object-fit:cover;width:100%;'src=" + data[i].img + ">";
+function get_webtoon_by_service(service_num) {
+  var webtoon_contents = document.getElementById("webtoon_contents");
+  webtoon_contents.innerHTML = "";
+  var target_api = api_url + week[today.getDay()];
+  ajax_get(target_api, function (data) {
+    for (i = 0; i < data.length; i++) {
+      if (service_num == 0) {
+        get_webtoon();
+      } else if (data[i].service == service_num) {
+        get_webtoon();
       }
-      new_dd.innerHTML =
-        "<p style=font-size:0.8em; text-align:center; margin:0; color:#E50914>" +
-        data[i].title +
-        "</p><p style=font-size:0.5em; text-align:center; color:#E50914>" +
-        data[i].artist +
-        "</p>";
+      function get_webtoon() {
+        var webtoon_link = document.createElement("a");
+        webtoon_link.href = data[i].url;
+        var new_dt = document.createElement("dt");
+        new_dt.classList.add("webtoon_container");
+        var new_div = document.createElement("div");
+        new_div.classList.add("square");
+        var img_container = document.createElement("div");
+        img_container.classList.add("content");
+        var img_text = document.createElement("div");
+        img_text.classList.add("icon");
+        var new_dd = document.createElement("dd");
+        new_dd.classList.add("webtoon_info");
+        if (data[i].img == null) {
+          img_container.innerHTML =
+            "<img src=img/noimg.jpg width=25% height=15%>";
+        } else {
+          img_container.innerHTML =
+            "<img style='object-fit:cover;width:100%;'src=" + data[i].img + ">";
+        }
+        new_dd.innerHTML =
+          "<p style=font-size:0.8em; text-align:center; margin:0; color:#E50914>" +
+          data[i].title +
+          "</p><p style=font-size:0.5em; text-align:center; color:#E50914>" +
+          data[i].artist +
+          "</p>";
 
-      switch (data[i].service) {
-        case 1:
-          img_text.innerHTML = "<img src=img/naver.png width=30%>";
-          break;
-        case 2:
-          img_text.innerHTML = "<img src=img/daum.png width=30%>";
-          break;
+        switch (data[i].service) {
+          case 1:
+            img_text.innerHTML = "<img src=img/naver.png width=30%>";
+            break;
+          case 2:
+            img_text.innerHTML = "<img src=img/daum.png width=30%>";
+            break;
+        }
+        webtoon_contents.appendChild(webtoon_link);
+        webtoon_link.appendChild(new_dt);
+        new_dt.appendChild(new_div);
+        new_div.appendChild(img_container);
+        img_container.appendChild(img_text);
+        new_dt.appendChild(new_dd);
       }
-      webtoon_contents.appendChild(webtoon_link);
-      webtoon_link.appendChild(new_dt);
-      new_dt.appendChild(new_div);
-      new_div.appendChild(img_container);
-      img_container.appendChild(img_text);
-      new_dt.appendChild(new_dd);
     }
-  }
-});
+  });
+}
