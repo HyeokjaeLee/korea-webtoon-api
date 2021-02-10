79b21346f0c53eb2f5a2ffe764e60bbc77a0575d
leehyeokjae <leehyeokjae97@gmail.com>
2021-02-11 01:01:15 +0900

    ver 1.6.11
    bug fix

--- dist/Toy-project-API/src/modules/korea-covid-19-api/base_modules.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.covid19_api_url = void 0;
-var covid19_api_url = function (service_key, from, to, middle_url) {
-    return "http://openapi.data.go.kr/openapi/service/rest/Covid19/" + middle_url + "?serviceKey=" + service_key + "&pageNo=1&numOfRows=1&startCreateDt=" + from + "&endCreateDt=" + to;
-};
-exports.covid19_api_url = covid19_api_url;
