999dd3a206827cacbac962b5c349c28e1b6511cb
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-04-11 00:43:29 +0900

    Update naver_webtoon.ts

--- test/naver_webtoon.ts
+++ test/naver_webtoon.ts
@@ -16,26 +16,33 @@ const get_finished_webtoon = () => {
   #ct > div.section_list_toon > ul > li:nth-child(1) > a > div.thumbnail > img
 }*/
 
-const test = () =>{
+
+const get_a_page_webtoon = (type:string, query_type:string, query_index:number|string) => {
   return new Promise(function (resolve, reject) {
     request(
-      `${naver_webtoon_url}/webtoon/finish.nhn?page=${1}`,
+      `${naver_webtoon_url}/webtoon/${type}.nhn?${query}`,
       (err: any, response: any, body: any) => {
         const $ = load(body);
-        const list_selector = $("#ct > div.section_list_toon > ul > li > a")
-          const a_webtoon_selector = list_selector.map((index,element) =>{
-            console.log({
-              title:$(element).find(".title").text(),
-              artist:$(element).find(".author").text(),
-              url:naver_webtoon_url+$(element).attr("href"),
-              img:$(element).find("div.thumbnail > img").attr("src"),
-              service:"naver",
-              state:"완결",
-              weekday:7
-            })
-          });
+        const list_selector = $("#ct > div.section_list_toon > ul > li > a");
+        const a_page_webtoon_info = list_selector.map((index, element) => {
+          if(type=="finished"){
+
+          }
+          const state_type = $(element)
+            .find("div.info > span.detail > span.bullet.up > span")
+            .text();
+          return {
+            title: $(element).find(".title").text(),
+            artist: $(element).find(".author").text(),
+            url: naver_webtoon_url + $(element).attr("href"),
+            img: $(element).find("div.thumbnail > img").attr("src"),
+            service: "naver",
+            state: "완결",
+            weekday: 7,
+          };
+        });
+        resolve(a_page_webtoon_info);
       }
     );
   });
-}
-test();
+};
