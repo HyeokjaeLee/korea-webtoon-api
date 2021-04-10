c1c2c53f1a1a9f88278beef6725c768231ddcfa2
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-04-10 23:47:33 +0900

    test
    naver webtoon crawler test

---
+++ test/naver_webtoon.ts
@@ -0,0 +1,41 @@
+const request = require("request-promise-native");
+import { load } from "cheerio";
+
+const naver_webtoon_url = "https://m.comic.naver.com";
+
+const get_finished_webtoon = () => {
+  for (
+    let page_index = 1, page_count = 1;
+    page_index <= page_count;
+    page_index++
+  ) {}
+  //const finished_webtoon_url = `${naver_webtoon_url}/webtoon/finish.nhn?page=${page_index}`
+};
+
+/*class get_a_page_webtoon{
+  #ct > div.section_list_toon > ul > li:nth-child(1) > a > div.thumbnail > img
+}*/
+
+const test = () =>{
+  return new Promise(function (resolve, reject) {
+    request(
+      `${naver_webtoon_url}/webtoon/finish.nhn?page=${1}`,
+      (err: any, response: any, body: any) => {
+        const $ = load(body);
+        const list_selector = $("#ct > div.section_list_toon > ul > li > a")
+          const a_webtoon_selector = list_selector.map((index,element) =>{
+            console.log({
+              title:$(element).find(".title").text(),
+              artist:$(element).find(".author").text(),
+              url:naver_webtoon_url+$(element).attr("href"),
+              img:$(element).find("div.thumbnail > img").attr("src"),
+              service:"naver",
+              state:"완결",
+              weekday:7
+            })
+          });
+      }
+    );
+  });
+}
+test();
