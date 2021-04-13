867b09f3af552d88ad763ad01b8d862b8440c407
Hyeokjae Lee (Nagle) <71566740+HyeokjaeLee@users.noreply.github.com>
2021-04-13 23:12:47 +0900

    test
    Open Insider data and yahoo stock data linkage test

--- test/get_openInsider_data.ts
+++ test/get_openInsider_data.ts
@@ -1,21 +1,7 @@
 const request = require("request-promise-native");
 import { load } from "cheerio";
-//import { string2date } from "../../modules/FormatConversion";
-//import type { A_trade_data } from "../../modules/types";
 
-export const string2date = (string_date: string) => {
-  const strArr: string[] = string_date.split("-");
-  const numArr: number[] = [];
-  for (let i = 0; i < 3; i++) {
-    numArr[i] = Number(strArr[i]);
-  }
-  const date: Date = new Date(numArr[0], numArr[1] - 1, numArr[2]);
-  return date;
-};
-
-const $2num = (string_data: string) =>
-  Number(string_data.replace("$", "").replace(/,/gi, ""));
-const _get_buy_data = async () => {
+const get_buy_data = async () => {
   const openInsider_data: any[] = [];
   const openInsiderURL = "http://openinsider.com/insider-purchases-25k";
   await request(
@@ -31,7 +17,7 @@ const _get_buy_data = async () => {
           trade_date: get_a_data(3),
           company_name: get_a_data(5),
           insider_name: get_a_data(6),
-          price: get_a_data(9),
+          price: Number(get_a_data(9).replace("$", "")),
           qty: Number(get_a_data(10).replace(/,/gi, "")),
           owned: Number(get_a_data(11).replace(/,/gi, "")),
           value: Number(get_a_data(13).replace("$", "").replace(/,/gi, "")),
@@ -42,34 +28,61 @@ const _get_buy_data = async () => {
   );
   console.log(openInsider_data);
 };
-//#tablewrapper > table > tbody > tr:nth-child(1) > td:nth-child(4) > b > a
-_get_buy_data();
-/*const get_buy_data = (url: string):A_trade_data[]|any => {
-  return new Promise((resolve) => {
-    request(url, (err: string, response: object[], body: string) => {
-      $ = load(body);
-      let data: A_trade_data[] = [];
-      const column_count: number = $("#tablewrapper > table > tbody > tr").length;
-      for (let i = 0; i < column_count; i++) {
-        data.push(get_a_data(i));
-      }
-      resolve(data);
-    });
-  });
-};
-const get_a_data_part = (column_num: number, row_num: number) => {
-  return $(".tinytable").find("tbody").find("tr").eq(column_num).find("td").eq(row_num).text();
+get_buy_data();
+const yahooStockPrices = require("yahoo-stock-prices");
+
+const newDate = new Date();
+const today = {
+  year: newDate.getFullYear(),
+  month: newDate.getMonth(),
+  day: newDate.getDate(),
 };
 
-const get_a_data = (column_num: number): A_trade_data => ({
-  ticker: get_a_data_part(column_num, 3).replace(/ /gi, ""),
-  trade_date: string2date(get_a_data_part(column_num, 2)),
-  company_name: get_a_data_part(column_num, 4),
-  insider_name: get_a_data_part(column_num, 5),
-  price: $2num(get_a_data_part(column_num, 8)),
-  qty: $2num(get_a_data_part(column_num, 9)),
-  owned: $2num(get_a_data_part(column_num, 10)),
-  value: $2num(get_a_data_part(column_num, 12)),
-});
+const get_yahooStockPrices = async (ticker: string) =>
+  await yahooStockPrices.getHistoricalPrices(
+    0,
+    0,
+    0,
+    today.month,
+    today.day,
+    today.year
+  );
 
-export default get_buy_data;*/
+  const checkEmpty = (data: any) => {
+    if (typeof data == undefined || data == null || data == "") return false;
+    else return true;
+  };
+
+class StockData {
+  private newDate = new Date();
+  private today = {
+    year: newDate.getFullYear(),
+    month: newDate.getMonth(),
+    day: newDate.getDate(),
+  };
+  public errorTicker=[];
+  private unique_tickerList;
+  private get_yahooStockPrices = async (ticker: string) =>
+    await yahooStockPrices.getHistoricalPrices(
+      0,
+      0,
+      0,
+      today.month,
+      today.day,
+      today.year
+    );
+    constructor(openInsider_data){
+        this.unique_tickerList=Array.from(new Set(openInsider_data.map((data)=>data.ticker)));
+    }
+    public get_stock_data=()=>{
+        const stock_data = this.unique_tickerList.map(async (ticker)=>{
+            try{
+               const stock_data = await get_yahooStockPrices(ticker);
+               return{ticker:ticker,data:stock_data}
+            }
+            catch(e){
+                this.errorTicker.push(ticker);
+            }
+        })
+    }
+}
