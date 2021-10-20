f830022e0fc5eac4c133d91960a415bae2b10bea
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-10-21 08:31:35 +0900

    📦️ feat: ver 2.0.0

--- dist/components/naver-crawler.js
+++ dist/components/naver-crawler.js
@@ -13,12 +13,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.naver_crawler = void 0;
-const base_data_1 = require("../data/base-data");
 const request_promise_native_1 = __importDefault(require("request-promise-native"));
 const cheerio_1 = require("cheerio");
 const naver_webtoon_url = "https://m.comic.naver.com";
-const naver_crawler = () => __awaiter(void 0, void 0, void 0, function* () { return (yield get_finished_webtoon()).concat(yield get_weekly_webtoon()); });
+const weekday = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const fs_1 = __importDefault(require("fs"));
+const naver_crawler = () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`Naver-Webtoon crawler has started(${new Date()})`);
+    const weeklyWebtoonData = yield get_weekly_webtoon();
+    const finishedWebtoonData = yield get_finished_webtoon();
+    console.log("Naver-Webtoon 정보 크롤링 완료");
+    fs_1.default.writeFileSync("../data/naver-weekly-webtoon.json", JSON.stringify(weeklyWebtoonData));
+    fs_1.default.writeFileSync("../data/naver-finished-webtoon.json", JSON.stringify(finishedWebtoonData));
+    console.log("Naver-Webtoon 정보 저장 완료");
+    return { weeklyWebtoonData, finishedWebtoonData };
+});
 exports.naver_crawler = naver_crawler;
+(0, exports.naver_crawler)();
 function get_page_count() {
     return new Promise(function (resolve, reject) {
         (0, request_promise_native_1.default)(naver_webtoon_url + "/webtoon/finish.nhn?page=1", (err, response, body) => {
@@ -34,22 +45,17 @@ function get_webtoon_of_one_page(type, query_type, week_num) {
             const $ = (0, cheerio_1.load)(body);
             const list_selector = $("#ct > div.section_list_toon > ul > li > a");
             list_selector.map((index, element) => {
-                const state_type = $(element).find("div.info > span.detail > span > span").eq(0).text();
-                const state = state_type == "휴재"
-                    ? "휴재"
-                    : state_type == "up"
-                        ? "UP"
-                        : type == "finish"
-                            ? "완결"
-                            : "";
+                const adult = $(element).find("div.thumbnail > span > span").attr("class") === "badge adult"
+                    ? true
+                    : false;
                 a_page_webtoon_info.push({
                     title: $(element).find(".title").text(),
                     artist: $(element).find(".author").text(),
                     url: naver_webtoon_url + $(element).attr("href"),
                     img: $(element).find("div.thumbnail > img").attr("src"),
                     service: "naver",
-                    state: state,
                     weekday: week_num,
+                    adult: adult,
                 });
             });
         });
@@ -68,7 +74,7 @@ function get_finished_webtoon() {
 }
 function get_weekly_webtoon() {
     return __awaiter(this, void 0, void 0, function* () {
-        const weeklyWebtoonArr = base_data_1.weekday.map((_weekday, _week_num) => get_webtoon_of_one_page("weekday", `week=${_weekday}`, _week_num));
+        const weeklyWebtoonArr = weekday.map((_weekday, _week_num) => get_webtoon_of_one_page("weekday", `week=${_weekday}`, _week_num));
         yield Promise.all(weeklyWebtoonArr);
         const result = [
             ...(yield weeklyWebtoonArr[0]),
