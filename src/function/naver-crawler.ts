44b92106a43e4747cfecb73d0d5d23b40a00c2be
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-10-23 12:37:02 +0900

    ✨ feat: naver 크롤링 정보 추가
    * 신규, 휴재, 업데이트 여부 정보 추가

--- src/function/naver-crawler.ts
+++ src/function/naver-crawler.ts
@@ -1,29 +1,14 @@
 import type { Webtoon } from '../types/webtoon';
 import axios from 'axios';
 import { load } from 'cheerio';
-const naver_webtoon_url = 'https://m.comic.naver.com';
+
 import * as fs from 'fs';
 const load_$ = async (url: string) => {
   const html: { data: string } = await axios.get(url);
   return load(html.data);
 };
 
-export default async function naver_crawler() {
-  console.log(`naver crawler start (${new Date()})`);
-  const weekdayWebtoon = await get_weekdayWebtoon();
-  fs.writeFileSync(
-    '../../data/naver-weekday-webtoon.json',
-    JSON.stringify(weekdayWebtoon),
-  );
-  const finishedWebtoon = await get_finishedWebtoon();
-  fs.writeFileSync(
-    '../../data/naver-finished-webtoon.json',
-    JSON.stringify(finishedWebtoon),
-  );
-  console.log(`naver crawler end (${new Date()}`);
-  return { weekdayWebtoon, finishedWebtoon };
-}
-
+const naver_webtoon_url = 'https://m.comic.naver.com';
 /**한 url에 표시되는 모든 웹툰 정보를 가지고오는 함수
  * @param type 웹툰의 종류(weekday, finish)
  * @param query 웹툰의 페이지 정보(week=mon, page=1)
@@ -39,11 +24,25 @@ async function get_webtoonData(
   const baseSelector = $('#ct > div.section_list_toon > ul > li > a');
   return baseSelector
     .map((index, element) => {
-      const adult =
+      const isAdult =
         $(element).find('div.thumbnail > span > span').attr('class') ===
         'badge adult'
           ? true
           : false;
+      let isNew = false;
+      let isRest = false;
+      let isUp = false;
+      if (type === 'weekday') {
+        const detailSelector = $(element).find('div.info > span.detail > span');
+        const detailInfo = detailSelector
+          .map((index, element) =>
+            $(element).attr('class').replace('bullet ', ''),
+          )
+          .get();
+        isNew = detailInfo.includes('new');
+        isRest = detailInfo.includes('break');
+        isUp = detailInfo.includes('up');
+      }
       return {
         title: $(element).find('.title').text(),
         author: $(element).find('.author').text(),
@@ -53,7 +52,7 @@ async function get_webtoonData(
         weekday: weeknum,
         additional: {
           new: false,
-          adult: adult,
+          adult: isAdult,
           rest: false,
           up: false,
         },
@@ -64,21 +63,12 @@ async function get_webtoonData(
 
 async function get_weekdayWebtoon() {
   const weekdayArr = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-  const weekdayWebtoon = await Promise.all(
+  return await Promise.all(
     weekdayArr.map(
       async (weekday, weeknum) =>
         await get_webtoonData('weekday', `week=${weekday}`, weeknum),
     ),
   );
-  return [
-    ...weekdayWebtoon[0],
-    ...weekdayWebtoon[1],
-    ...weekdayWebtoon[2],
-    ...weekdayWebtoon[3],
-    ...weekdayWebtoon[4],
-    ...weekdayWebtoon[5],
-    ...weekdayWebtoon[6],
-  ];
 }
 
 async function get_finishedWebtoon() {
@@ -93,4 +83,20 @@ async function get_finishedWebtoon() {
     );
   }
   return finishedWebtoon;
+}
+
+export default async function naver_crawler() {
+  console.log(`naver crawler start (${new Date()})`);
+  const weekdayWebtoon = await get_weekdayWebtoon();
+  fs.writeFileSync(
+    '../../data/naver-weekday-webtoon.json',
+    JSON.stringify(weekdayWebtoon),
+  );
+  const finishedWebtoon = await get_finishedWebtoon();
+  fs.writeFileSync(
+    '../../data/naver-finished-webtoon.json',
+    JSON.stringify(finishedWebtoon),
+  );
+  console.log(`naver crawler end (${new Date()}`);
+  return { weekdayWebtoon, finishedWebtoon };
 }
