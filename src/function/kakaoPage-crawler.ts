aabbe40e01713f2f6939622a35ff824970bd614d
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-11-01 13:16:26 +0900

    ✨ feat: 요일별 쿼리 요청 허용
    * 전체 코드 구조 개선
    * 반복 코드 제거를 위해 웹툰 분류 위치를 service.ts > controller.ts 변경
    * weekdayWebtoon > weekWebtoon 표기 변경

--- src/function/kakaoPage-crawler.ts
+++ src/function/kakaoPage-crawler.ts
@@ -1,4 +1,4 @@
-import type { Webtoon } from '../types/webtoon';
+import { Webtoon } from '../types/webtoon';
 import axios from 'axios';
 import * as _ from 'lodash';
 
@@ -12,10 +12,10 @@ interface KakaoPageWebtoon {
 
 async function get_webtoonData(
   page: number,
-  weekday: number,
+  week: number,
 ): Promise<Webtoon[] | false> {
   const kakaoPageURL = `https://api2-page.kakao.com/api/v2/store/day_of_week_top/list?category=10&subcategory=0&page=${page}&day=${
-    weekday === 7 ? 12 : weekday + 1
+    week === 7 ? 12 : week + 1
   }`;
   const { data }: any = await axios.get(kakaoPageURL);
   const webtoonDataArr: KakaoPageWebtoon[] = data.list;
@@ -29,7 +29,7 @@ async function get_webtoonData(
         url: `https://page.kakao.com/home?seriesId=${webtoonData.series_id}`,
         img: `http://dn-img-page.kakao.com/download/resource?kid=${webtoonData.image}&filename=th2`,
         service: 'kakao-page',
-        weekday: weekday,
+        week: week,
         additional: {
           new: isNew,
           rest: false, //휴재중인 웹툰이 없어서 확인 불가
@@ -43,11 +43,11 @@ async function get_webtoonData(
   }
 }
 
-async function get_one_DayWebtoonData(weekday: number) {
+async function get_one_DayWebtoonData(weeknum: number) {
   const webtoonData: Webtoon[] = [];
   let page = 0;
   while (true) {
-    const one_page_webtoonData = await get_webtoonData(page, weekday);
+    const one_page_webtoonData = await get_webtoonData(page, weeknum);
     if (!one_page_webtoonData) break;
     else {
       webtoonData.push(...one_page_webtoonData);
@@ -58,13 +58,13 @@ async function get_one_DayWebtoonData(weekday: number) {
 }
 
 export default async function kakaoPage_crawler() {
-  console.log(`kakao-page crawler start (${new Date()})`);
-  const weekdayWebtoon = [];
+  console.log('kakao-page crawler start');
+  const weekWebtoon = [];
   for (let i = 0; i < 7; i++) {
-    weekdayWebtoon.push(await get_one_DayWebtoonData(i));
+    weekWebtoon.push(await get_one_DayWebtoonData(i));
   }
   const finishedWebtoon = await get_one_DayWebtoonData(7);
 
-  console.log(`kakao-page crawler end (${new Date()}`);
-  return { weekdayWebtoon, finishedWebtoon };
+  console.log('kakao-page crawler end');
+  return { weekWebtoon, finishedWebtoon };
 }
