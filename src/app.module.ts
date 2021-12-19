799a3cc0211db23dc1d94f12d12eac0a90acaa8e
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-12-19 20:52:39 +0900

    🗃️ refactor: mongoDB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { PASSWORD } from '../temp-password';
+const MONGO_DB_URI = `mongodb+srv://nagle:${PASSWORD}@cluster0.uko3q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGO_DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
