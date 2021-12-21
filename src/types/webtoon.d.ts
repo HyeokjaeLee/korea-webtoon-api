e8869aad6127a0969de9b1fffb86449bca7c828f
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-12-21 21:49:08 +0900

    🔧 build: 기본 타입 생성 및 컴파일 옵션 수정

---
+++ src/types/webtoon.d.ts
@@ -0,0 +1,39 @@
+namespace Webtoon {
+  /**플랫폼 표준 웹툰 정보
+   * @title 제목
+   * @author 작가명
+   * @img 대표이미지
+   * @url 웹툰 url
+   */
+  interface Basic {
+    title: string;
+    author: string;
+    url: string;
+    img: string;
+    service: string;
+  }
+
+  /**웹툰 추가 정보
+   * @new 신규
+   * @rest 휴재
+   * @up 업데이트
+   * @adult 성인작품
+   */
+  interface Additional {
+    new: boolean;
+    rest: boolean;
+    up: boolean;
+    adult: boolean;
+  }
+
+  interface CrawlerOutput extends Basic {
+    week: number;
+    additional: Additional;
+  }
+
+  interface CreateDto extends Basic {
+    id: number;
+    week: number[];
+    additional: Additional;
+  }
+}
