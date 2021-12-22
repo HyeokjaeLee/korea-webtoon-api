028ef968f75b459c4e0afaaf8cd24c9dbb4fee06
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-12-23 06:34:59 +0900

    🚚 refactor: 폴더명 변경
    * webtoon > webtoons

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { PASSWORD } from '../temp-password';
 const MONGO_DB_URI = `mongodb+srv://nagle:${PASSWORD}@cluster0.uko3q.mongodb.net/test?retryWrites=true&w=majority`;
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { WebtoonsModule } from './webtoon/webtoons.module';
+import { WebtoonsModule } from './webtoons/webtoons.module';
 
 @Module({
   imports: [MongooseModule.forRoot(MONGO_DB_URI), WebtoonsModule],
