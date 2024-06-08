1ec32032156b8061077fe6b46cd9f59201d2152f
HyeokjaeLee <leehyeokjae97@gmail.com>
2024-06-08 19:02:11 +0900

    chore: express 실행 환경 구축

--- src/route.ts
+++ src/route.ts
@@ -1,3 +1,12 @@
-/** @format */
+import express from 'express';
 
-import app from "express";
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
